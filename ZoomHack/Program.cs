cd651e0f600dccc3bacd115dafc3895253bed16c
Wiktor <wicia_6@wp.pl>
2015-08-26 23:24:10 +0200

    ZoomHack push

---
+++ ZoomHack/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ZoomHack
+{
+    static class Program
+    {
+        public const string Version = "1.3.3.*";
+        static LeagueSharp.Common.Menu _menu;
+        static void Main()
+        {
+            LeagueSharp.Common.CustomEvents.Game.OnGameLoad += eventArgs =>
+            {
+                _menu = new LeagueSharp.Common.Menu("[ST] ZoomHack", "SparkTech", true);
+                var zoomHack = _menu.AddItem((new LeagueSharp.Common.MenuItem("zoomhack", "Active!")).SetValue(true));
+                zoomHack.SetValue(LeagueSharp.Hacks.ZoomHack);
+                zoomHack.ValueChanged += delegate (object sender, LeagueSharp.Common.OnValueChangeEventArgs args)
+                    {
+                        LeagueSharp.Hacks.ZoomHack = args.GetNewValue<bool>();
+                    };
+                _menu.AddToMainMenu();
+            };
+        }
+    }
+}
---
+++ ZoomHack/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("Simple ZoomHack")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple ZoomHack")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("52e46eb2-e5d9-43d0-9d9a-40b17fe5e81c")]
+[assembly: AssemblyVersion(ZoomHack.Program.Version)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
