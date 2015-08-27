733a188d44a32ae7f4cd915bd8667324c30e2114
Wiktor <wicia_6@wp.pl>
2015-08-27 18:02:34 +0200

    Fix

---
+++ ZoomHack/Updater.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using LeagueSharp.Common;
+using SparkTech;
+
+namespace ZoomHack
+{
+    class Updater
+    {
+        private static UpdateChecker _updateChecker;
+
+        internal static void Init()
+        {
+            if (Settings.UpdateCheck)
+            {
+                Utility.DelayAction.Add(Settings.UpdateCheckDelay, () =>
+                {
+                    _updateChecker = new UpdateChecker(Assembly.GetExecutingAssembly().GetName().Name);
+                    _updateChecker.onGetVersionCompleted += AssemblyUtil_onGetVersionCompleted;
+                    _updateChecker.GetLastVersionAsync();
+                });
+            }
+        }
+
+        private static void AssemblyUtil_onGetVersionCompleted(OnGetVersionCompletedArgs args)
+        {
+            if (args.LastAssemblyVersion == Assembly.GetExecutingAssembly().GetName().Version.ToString())
+            {
+                if (Settings.SkipNoUpdate)
+                {
+                    return;
+                }
+                Comms.Print("You have the latest version of \"" + Program.AssemblyName + "\"");
+                return;
+            }
+            Comms.Print("A new version of \"" + Program.AssemblyName + "\" is available!");
+        }
+    }
+}
