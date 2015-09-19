60f846525b1b5d8b5d6b561c74a6f5f17f818ac1
Wiciaki <wicia_6@wp.pl>
2015-09-19 15:05:34 +0200

    todo : Fix

--- SparkTech/STMenu.cs
+++ SparkTech/STMenu.cs
@@ -19,96 +19,108 @@
     [SuppressMessage("ReSharper", "ConvertPropertyToExpressionBody")]
     public class STMenu
     {
-        public static Menu LibraryMenu = new Menu("[ST] Core Features", "SparkTech", true);
-
-        private static readonly Menu CheatsMenu = new Menu("Cheats", "st_cheats_head");
+        public static Menu LibraryMenu;
 
         static STMenu()
         {
             try
             {
+                LibraryMenu = new Menu("[ST] Core Features", "SparkTech", true);
+
                 #region F5Settings
 
-                {
+                /*
+
                     var F5Settings = new Menu("F5Settings", "F5Settings");
                     {
                         var st_f5settings_zoomhack = F5Settings.AddItem((new MenuItem("st_f5settings_zoomhack", "Load ZoomHack (unsafe!)"))).SetValue(false);
                         st_f5settings_zoomhack.ValueChanged += (s, a) => Settings.LoadZoomHack = a.GetNewValue<bool>(); // redundant?
                     }
                     LibraryMenu.AddSubMenu(F5Settings);
-                }
+                
+                */
 
                 #endregion
 
                 #region Orbwalking
 
-                {
+                
                     var orbmenu = new Menu("BrianWalker", "lelelelell");
                     BrianWalker.Init(orbmenu);
                     LibraryMenu.AddSubMenu(orbmenu);
-                }
+                
 
                 #endregion
 
                 new Extensions();
 
                 #region Cheats
-
+                /*
+                Menu cheatsMenu = new Menu("Cheats", "st_cheats_head");
                 Settings.LoadZoomHack = LibraryMenu.Item("st_f5settings_zoomhack").GetValue<bool>(); // ???
 
                 if (Settings.LoadZoomHack)
                 {
-                    var zoomHack = CheatsMenu.AddItem(new MenuItem("st_cheats_zoomhack_enable", "Enable ZoomHack").SetValue(false));
+                    var zoomHack =
+                        cheatsMenu.AddItem(new MenuItem("st_cheats_zoomhack_enable", "Enable ZoomHack").SetValue(false));
                     zoomHack.ValueChanged += (s, a) =>
-                    {
-                        Settings.ChValue1 = a.GetNewValue<bool>();
-                        Utility.DelayAction.Add(10, () => Hacks.ZoomHack = Settings.ChValue1 && Settings.ChValue2);
-                    };
+                        {
+                            Settings.ChValue1 = a.GetNewValue<bool>();
+                            Utility.DelayAction.Add(10, () => Hacks.ZoomHack = Settings.ChValue1 && Settings.ChValue2);
+                        };
 
-                    CheatsMenu.AddItem((new MenuItem("st_separator_" + NextNumber, "")));
+                    cheatsMenu.AddItem((new MenuItem("st_separator_" + NextNumber, "")));
 
-                    var confirmation = CheatsMenu.AddItem(new MenuItem("st_cheats_zoomhack_confirmation", "Confirm your selection").SetValue(false));
+                    var confirmation =
+                        cheatsMenu.AddItem(
+                            new MenuItem("st_cheats_zoomhack_confirmation", "Confirm your selection").SetValue(false));
                     confirmation.SetValue(Hacks.ZoomHack);
                     confirmation.ValueChanged += (s, a) =>
-                    {
-                        Settings.ChValue2 = a.GetNewValue<bool>();
-                        Utility.DelayAction.Add(10, () => Hacks.ZoomHack = Settings.ChValue1 && Settings.ChValue2);
-                    };
+                        {
+                            Settings.ChValue2 = a.GetNewValue<bool>();
+                            Utility.DelayAction.Add(10, () => Hacks.ZoomHack = Settings.ChValue1 && Settings.ChValue2);
+                        };
 
-                    CheatsMenu.AddItem(new MenuItem("st_separator_" + NextNumber, "Warning: ZoomHack is extremely unsafe!"));
-                    CheatsMenu.AddItem(new MenuItem("st_separator_" + NextNumber, ""));
+                    cheatsMenu.AddItem(
+                        new MenuItem("st_separator_" + NextNumber, "Warning: ZoomHack is extremely unsafe!"));
+                    cheatsMenu.AddItem(new MenuItem("st_separator_" + NextNumber, ""));
 
-                    var dcIndicator = CheatsMenu.AddItem(new MenuItem("st_cheats_dci", "Disable Cast Indicator").SetValue(false));
+                    var dcIndicator =
+                        cheatsMenu.AddItem(new MenuItem("st_cheats_dci", "Disable Cast Indicator").SetValue(false));
                     dcIndicator.SetValue(Hacks.DisableCastIndicator);
                     dcIndicator.ValueChanged += (s, a) => Hacks.DisableCastIndicator = a.GetNewValue<bool>();
 
-                    CheatsMenu.AddItem(new MenuItem("st_separator_" + NextNumber, "It disables the display of your skill ranges"));
+                    cheatsMenu.AddItem(
+                        new MenuItem("st_separator_" + NextNumber, "It disables the display of your skill ranges"));
 
-                    LibraryMenu.AddSubMenu(CheatsMenu);
+                    LibraryMenu.AddSubMenu(cheatsMenu);
                 }
-
+                */
                 #endregion
 
-                #region Bools
-
-                Settings.ChValue1 = LibraryMenu.Item("st_cheats_zoomhack_enable").GetValue<bool>();
-                Settings.ChValue2 = LibraryMenu.Item("st_cheats_zoomhack_confirmation").GetValue<bool>();
-
-                #endregion
-
+                
                 LibraryMenu.AddItem(new MenuItem("onupdatedelay", "Delay in checking for menu changes")).SetValue(new Slider(300, 0, 1000));
 
                 LibraryMenu.AddToMainMenu();
-                Updater.Check("SparkTech");
+
+                #region Bools
+
+                // Settings.ChValue1 = LibraryMenu.Item("st_cheats_zoomhack_enable").GetValue<bool>();
+                // Settings.ChValue2 = LibraryMenu.Item("st_cheats_zoomhack_confirmation").GetValue<bool>();
+
+                #endregion
+
+                // Updater.Check("SparkTech");
                 // Game.OnUpdate += Settings.OnSettingsChange;
             }
             catch (Exception e)
             {
-                Comms.Print("SparkTech - An error occured while injecting. Press F5 to retry.");
+                Comms.Print("An error occured while injecting. Press F5 to retry.");
                 Comms.Print("Menu not built!", true);
                 Comms.Print(e.ToString(), true);
             }
         }
+
         #region Misc
 
         private static int tempNumber;
