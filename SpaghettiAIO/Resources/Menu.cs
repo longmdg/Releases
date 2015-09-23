7dc92bb30de47744aa2ff9874f441ace275fd128
Wiciaki <wicia_6@wp.pl>
2015-09-23 15:03:46 +0200

    wip

---
+++ SpaghettiAIO/Resources/Menu.cs
@@ -0,0 +1,34 @@
+﻿namespace SparkTech.Resources
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using System.Diagnostics.CodeAnalysis;
+
+    [SuppressMessage("ReSharper", "UseNullPropagation")]
+    internal class Menu
+    {
+        #region Event
+
+        internal delegate void OnMenuCreatedH();
+        internal static event OnMenuCreatedH OnMenuCreated;
+
+        private static void FireOnMenuCreated()
+        {
+            if (OnMenuCreated != null)
+            {
+                OnMenuCreated();
+            }
+        }
+
+        #endregion
+
+        static Menu()
+        {
+            FireOnMenuCreated();
+        }
+    }
+}
