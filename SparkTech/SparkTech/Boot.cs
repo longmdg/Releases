b1e80e6d04644abd69c49258bfea4aad39404d3d
Wiciaki <wicia_6@wp.pl>
2015-10-03 19:41:40 +0200

    Progress

--- SparkTech/SparkTech/Boot.cs
+++ SparkTech/SparkTech/Boot.cs
@@ -4,39 +4,12 @@
     using System.Diagnostics.CodeAnalysis;
 
     using LeagueSharp;
-    using LeagueSharp.Common;
 
     [SuppressMessage("ReSharper", "UseNullPropagation")]
     public static class Boot
     {
         public delegate void OnInitH(EventArgs args);
         public static event OnInitH OnInit;
-
-        static Boot()
-        {
-            Utility.DelayAction.Add(250, () =>
-                    {
-                        switch (Game.Mode)
-                        {
-                            case GameMode.Connecting:
-                                Resources.Connecting.Instance();
-                                break;
-                            case GameMode.Running:
-                            case GameMode.Paused:
-                                Resources.Menu.Instance();
-                                break;
-                            case GameMode.Finished:
-                                Console.WriteLine(@"Too late in the game to inject!");
-                                break;
-                            case GameMode.Exiting:
-                                Console.WriteLine(@"[ST] - Injection requirements not met - Incorrect GameMode!");
-                                break;
-                            default:
-                                Console.WriteLine(@"[ST] - Injection failed - Unknown GameMode");
-                                break;
-                        }
-                    });
-        }
 
         internal static void FireOnInit()
         {
@@ -46,6 +19,34 @@
             }
         }
 
-        public static void Initialize() { }
+        private static bool inited;
+
+        public static void Initialize()
+        {
+            if (inited)
+                return;
+            
+            inited = true;
+
+            switch (Game.Mode)
+            {
+                case GameMode.Connecting:
+                    Resources.Connecting.Instance();
+                    break;
+                case GameMode.Running:
+                case GameMode.Paused:
+                    Resources.Menu.Instance();
+                    break;
+                case GameMode.Finished:
+                    Console.WriteLine(@"Too late in the game to inject!");
+                    break;
+                case GameMode.Exiting:
+                    Console.WriteLine(@"[ST] - Injection requirements not met - Incorrect GameMode!");
+                    break;
+                default:
+                    Console.WriteLine(@"[ST] - Injection failed - Unknown GameMode");
+                    break;
+            }
+        }
     }
 }
